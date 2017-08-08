712cc3fe41ebc3a899857ba7ce4006344ec6dcf8
pieter-1983 <pieter.sterckx@cegeka.com>
2017-08-08 17:06:49 +0200

    refactored gamerunner to be able to test with a seed, wrote test with a seed and checked the complete consoleoutput

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -10,27 +10,29 @@ public class GameRunner {
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
-		Game aGame = new Game();
+		run(new Random());
 		
+	}
+
+	public static void run(Random random) {
+		Game aGame = new Game();
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
+
+			aGame.roll(random.nextInt(5) + 1);
+
+			if (random.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
+
+
 		} while (notAWinner);
-		
 	}
 }
--- java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -8,11 +8,138 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Random;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class GameRunnerTest {
+    @Test
+    public void runWithSeed() throws Exception {
+        //setup outputstream to capture console logs
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        //run the game with a seed
+        GameRunner gameRunner= new GameRunner();
+        gameRunner.run(new Random(1));
+        //result should be equal to this
+        String result="Chet was added\r\n" +
+                "They are player number 1\r\n" +
+                "Pat was added\r\n" +
+                "They are player number 2\r\n" +
+                "Sue was added\r\n" +
+                "They are player number 3\r\n" +
+                "Chet is the current player\r\n" +
+                "They have rolled a 1\r\n" +
+                "Chet's new location is 1\r\n" +
+                "The category is Science\r\n" +
+                "Science Question 0\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Chet now has 1 Gold Coins.\r\n" +
+                "Pat is the current player\r\n" +
+                "They have rolled a 3\r\n" +
+                "Pat's new location is 3\r\n" +
+                "The category is Rock\r\n" +
+                "Rock Question 0\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Pat now has 1 Gold Coins.\r\n" +
+                "Sue is the current player\r\n" +
+                "They have rolled a 5\r\n" +
+                "Sue's new location is 5\r\n" +
+                "The category is Science\r\n" +
+                "Science Question 1\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Sue now has 1 Gold Coins.\r\n" +
+                "Chet is the current player\r\n" +
+                "They have rolled a 5\r\n" +
+                "Chet's new location is 6\r\n" +
+                "The category is Sports\r\n" +
+                "Sports Question 0\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Chet now has 2 Gold Coins.\r\n" +
+                "Pat is the current player\r\n" +
+                "They have rolled a 4\r\n" +
+                "Pat's new location is 7\r\n" +
+                "The category is Rock\r\n" +
+                "Rock Question 1\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Pat now has 2 Gold Coins.\r\n" +
+                "Sue is the current player\r\n" +
+                "They have rolled a 5\r\n" +
+                "Sue's new location is 10\r\n" +
+                "The category is Sports\r\n" +
+                "Sports Question 1\r\n" +
+                "Question was incorrectly answered\r\n" +
+                "Sue was sent to the penalty box\r\n" +
+                "Chet is the current player\r\n" +
+                "They have rolled a 3\r\n" +
+                "Chet's new location is 9\r\n" +
+                "The category is Science\r\n" +
+                "Science Question 2\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Chet now has 3 Gold Coins.\r\n" +
+                "Pat is the current player\r\n" +
+                "They have rolled a 3\r\n" +
+                "Pat's new location is 10\r\n" +
+                "The category is Sports\r\n" +
+                "Sports Question 2\r\n" +
+                "Question was incorrectly answered\r\n" +
+                "Pat was sent to the penalty box\r\n" +
+                "Sue is the current player\r\n" +
+                "They have rolled a 3\r\n" +
+                "Sue is getting out of the penalty box\r\n" +
+                "Sue's new location is 1\r\n" +
+                "The category is Science\r\n" +
+                "Science Question 3\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Sue now has 2 Gold Coins.\r\n" +
+                "Chet is the current player\r\n" +
+                "They have rolled a 2\r\n" +
+                "Chet's new location is 11\r\n" +
+                "The category is Rock\r\n" +
+                "Rock Question 2\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Chet now has 4 Gold Coins.\r\n" +
+                "Pat is the current player\r\n" +
+                "They have rolled a 2\r\n" +
+                "Pat is not getting out of the penalty box\r\n" +
+                "Sue is the current player\r\n" +
+                "They have rolled a 1\r\n" +
+                "Sue is getting out of the penalty box\r\n" +
+                "Sue's new location is 2\r\n" +
+                "The category is Sports\r\n" +
+                "Sports Question 3\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Sue now has 3 Gold Coins.\r\n" +
+                "Chet is the current player\r\n" +
+                "They have rolled a 5\r\n" +
+                "Chet's new location is 4\r\n" +
+                "The category is Pop\r\n" +
+                "Pop Question 0\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Chet now has 5 Gold Coins.\r\n" +
+                "Pat is the current player\r\n" +
+                "They have rolled a 4\r\n" +
+                "Pat is not getting out of the penalty box\r\n" +
+                "Sue is the current player\r\n" +
+                "They have rolled a 3\r\n" +
+                "Sue is getting out of the penalty box\r\n" +
+                "Sue's new location is 5\r\n" +
+                "The category is Science\r\n" +
+                "Science Question 4\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Sue now has 4 Gold Coins.\r\n" +
+                "Chet is the current player\r\n" +
+                "They have rolled a 1\r\n" +
+                "Chet's new location is 5\r\n" +
+                "The category is Science\r\n" +
+                "Science Question 5\r\n" +
+                "Answer was correct!!!!\r\n" +
+                "Chet now has 6 Gold Coins.\r\n";
 
+        assertThat(outContent.toString(),is(equalTo(result)));
+    }
 
     @Test
     public void rollTwo_ShouldWriteExpectedOutputToConsole() throws Exception {
