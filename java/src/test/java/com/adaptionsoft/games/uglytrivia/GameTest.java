f1acd9a5656ee19f5249178025c0955d13b50a39
pieter-1983 <pieter.sterckx@cegeka.com>
2017-08-08 16:47:06 +0200

    I am feeling: tired Right now I have just finished:writing tests with mockito to verify console methods are called when calling Game methods with specific parameters Next up is: Should refactor the category lists

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -1,11 +1,81 @@
 package com.adaptionsoft.games.uglytrivia;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GameTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @InjectMocks
+    private Game game;
+    @Mock
+    private Console console;
+
+    @Test
+    public void add_shouldCallPrintAddingPlayerWithCorrectParameters() throws Exception {
+        game.add("jos");
+        verify(console).printAddingPlayer("jos",1);
+        game.add("jo");
+        verify(console).printAddingPlayer("jo",2);
+    }
+    @Test
+    public void roll_shouldCallPrintPlayerAndRollWithCorrectParameters() throws Exception {
+        game.add("jos");
+        game.roll(1);
+        verify(console).printPlayerAndRoll(1,"jos");
+    }
+
+    @Test
+    public void roll_shouldCallPrintPlayersLocationAndCategoryWithCorrectParameters() throws Exception {
+        game.add("jos");
+        game.roll(1);
+        verify(console).printPlayersLocationAndCategory("jos",1,"Science");
+    }
+    @Test
+    public void wasCorrectlyAnswered_shouldCallPrintCorrectAnswerAndGoldCoinsWithCorrectParameters() throws Exception {
+        game.add("jos");
+        game.wasCorrectlyAnswered();
+        verify(console).printCorrectAnswerAndGoldCoins("jos",1);
+    }
+    @Test
+    public void wrongAnswer_shouldCallPrintWrongAnswerAndSentToPenaltyBoxWithCorrectParameters() throws Exception {
+        game.add("jos");
+        game.wrongAnswer();
+        verify(console).printWrongAnswerAndSentToPenaltyBox("jos");
+    }
+    @Test
+    public void roll_WhenPlayerGetsOutOfPenaltyBox_shouldCallPrintGetsOutOfPenaltyBoxWithCorrectParameters()  throws Exception {
+        game.add("jos");
+        game.wrongAnswer();
+        game.roll(1);
+        verify(console).printGetsOutOfPenaltyBox("jos");
+    }
+    @Test
+    public void roll_WhenPlayerGetsOutOfPenaltyBox_shouldCallPlayersLocationAndCategoryWithCorrectParameters()  throws Exception {
+        game.add("jos");
+        game.wrongAnswer();
+        game.roll(1);
+        verify(console).printPlayersLocationAndCategory("jos",1,"Science");
+    }
+    @Test
+    public void roll_WhenPlayerDoesNotGetOutOfPenaltyBox_shouldCallPrintStaysInPenaltyBoxWithCorrectParameters()  throws Exception {
+        game.add("jos");
+        game.wrongAnswer();
+        game.roll(2);
+        verify(console).printStaysInPenaltyBox("jos");
+    }
+
+
     @Test
     public void addMethod_ShouldIncreasePlayersSize() throws Exception {
         Game test = new Game();
